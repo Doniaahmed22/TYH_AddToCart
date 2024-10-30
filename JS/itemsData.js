7f763b38da2b7d2bccb6d1184774c085651a208d
Doniaahmed22 <119499612+Doniaahmed22@users.noreply.github.com>
2024-10-30 13:56:48 +0300

    add to cart v2

---
+++ JS/itemsData.js
@@ -0,0 +1,39 @@
+const itemsData = [
+    
+    {
+        name: 'MacBook Pro 16-inch',
+        description: 'Apple MacBook Pro with a 16-inch Retina display.',
+        image: `../Images/Macbook Pro 16 with iPhone 11 Pro Max Mockup.jpeg`,
+        size: '16-inch',
+        quantity: 1,
+        price: 2500,
+        currency: 'USD'
+    },
+    {
+        name: 'iMac 24-inch',
+        description: 'Apple iMac with a stunning 24-inch 4.5K Retina display.',
+        image: `../Images/Apple_iMac_24__All-In-One_Computer__Apple_M1__8GB_RAM__256GB_SSD__macOS_Big_Sur__Blue__MJV93LL_A_-_Walmart_com-removebg-preview.png`, // Replace with the actual image path
+        size: '24-inch',
+        quantity: 1,
+        price: 1299,
+        currency: 'USD'
+    },
+    {
+        name: 'Mac Mini',
+        description: 'Apple Mac Mini with the powerful M1 chip.',
+        image: `../Images/fixed___hi-res_icons_for_the_new_Mac_mini_2018-removebg-preview.png`, // Replace with the actual image path
+        size: 'Compact Desktop',
+        quantity: 1,
+        price: 699,
+        currency: 'USD'
+    },
+    {
+        name: 'MacBook Keyboard Cover',
+        description: 'Silicone keyboard cover designed .',
+        image: '../Images/MacBook Laptop Case Cover (1).jpeg', // Replace with the actual image path
+        size: 'Fits 13-inch and 15-inch MacBooks',
+        quantity: 1,
+        price: 19,
+        currency: 'USD'
+    }
+];
